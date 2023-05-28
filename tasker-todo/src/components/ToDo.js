6f2ef8033276c9485a298029434a38e2b6ade6a5
unknown <marlonmich7@gmail.com>
2023-05-28 18:59:34 -0300

    added local storage to save tasks; adaptating addTask function to load storage

--- tasker-todo/src/components/ToDo.js
+++ tasker-todo/src/components/ToDo.js
@@ -1,39 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task"
 
 
-export default function ToDo(props){
+export default function ToDo(){
     const [taskId, setTaskId] = useState(0)
     const [tasks,setTasks] = useState([])
-    const [text, setText] = useState("")
+    const [searchBarText, setSearchBarText] = useState("")
+    const [storageNeedsUpdate, setStorageUpdate] = useState(false)
 
     const nextTaskId = () => {
         setTaskId(id => id+1)
         return taskId
     }
-
-    const addTask = () => {
+    
+    const addTask = (storageTasks) => {
+        if (!storageTasks[0]?.title.length > 0) storageTasks = [{title: "", state: "UNDONE"}]
         let id = nextTaskId()
-        setTasks(lastTasks => [...lastTasks, 
-            <Task key={id} id={id} task={{text:""}}
-            functionDeleteTask={deleteTask}></Task>])
+        let newTaskList = [...tasks]
+        storageTasks.forEach(task => {
+            newTaskList.push(
+                <Task key={id} task={{id, title:task.title, state:task.state}}
+                deleteTask={deleteTask} updateStorage={setStorageUpdate}></Task>
+            )
+            id++
+        })
+        setTaskId(id--)
+        setTasks(newTaskList)
     }
 
-    const deleteTask = (id) => {
-        setTasks (old => old.filter((task) => task?.props?.id !== id))
-    }
+    const deleteTask = (id) => setTasks (old => old.filter((task) => task?.props.task.id !== id))
+    const taskRenderer = () => {return [...tasks].filter((task) => task?.props.task.title.indexOf(searchBarText) !== -1)}
+    const updateSearchBar = (e) => setSearchBarText(e.target.value)
 
-    const taskRenderer = () => {
-        var tasksCopy = [...tasks]
-        tasksCopy = tasksCopy.filter((element) => element?.props.task.text.indexOf(text) !== -1)
-        return tasksCopy.map((task) => {return task})
-    }
+    useEffect(() => {
+        let tasksObj = JSON.parse(localStorage.getItem('Tasker.tasks'))
+        if (tasksObj === undefined || tasksObj === null || tasksObj === [] || tasks.length > 0) return
+        addTask(tasksObj)
+    })
 
-    const editSearchBar = (e) => setText(e.target.value)
+    useEffect(()=> {
+        setStorageUpdate(false)
+        let tasksObj = []
+        tasks.forEach(task => tasksObj.push({title: task.props.task.title,state: task.props.task.state}))
+        localStorage.setItem('Tasker.tasks', JSON.stringify(tasksObj))
+    } ,[storageNeedsUpdate, tasks])
     
     return (
         <div className='todo-section'>
-            <input onChange={editSearchBar} value={text} className='todo-search' placeholder='search your task here'></input>
+            <input className='todo-search' onChange={updateSearchBar} value={searchBarText} placeholder='search your task here'></input>
             <div className="todo">
                 {taskRenderer()}
             </div>
