de6ad54e239044673619f112f4e60ba35dc8499f
unknown <marlonmich7@gmail.com>
2023-05-28 18:57:34 -0300

    refactoring variable, const and function names

--- tasker-todo/src/components/Task.js
+++ tasker-todo/src/components/Task.js
@@ -2,31 +2,31 @@ import React, { useEffect, useRef, useState } from "react";
 
 
 export default function Task(props){
-    const [id] = useState(props.id)
+    const [id] = useState(props.task.id)
     const [editmode, setEditmode] = useState(false)
     const [state, setState] = useState(props.task.state ? props.task.state : "UNDONE")
-    const [task, setTask] = useState(props.task.text)
-    props.task.text = task
-    props.task.state = state
+    const [title, setTitle] = useState(props.task.title)
     const editInput = useRef(null)
 
-    const toggleEditmode = () => editmode ? setEditmode(false): setEditmode(true)
+    const toggleEditmode = () => editmode ? setEditmode(false): setEditmode(true); props.task.state = state
     const changeState = () => state === "DONE"? setState("UNDONE") : setState("DONE");
-    const deleteTask = () => props.functionDeleteTask(id)
-    const updateText = (e) => setTask(e.target.value); props.task.text = task
+    const deleteTask = () => props.deleteTask(id)
+    const updateText = (e) => setTitle(e.target.value); props.task.title = title
+    const updateStorage = () => props.updateStorage(true)
 
-    useEffect(()=> editmode ? editInput.current.focus() : ()=>{}, [editmode])
-
+    useEffect(()=> editmode ? editInput.current.focus() : undefined, [editmode])
+    useEffect(()=> updateStorage()) //[title, state]
+ 
     return (
         <div className="task">
             <div className="state-field"><button onClick={changeState} className={"state-button " + state}></button></div>
             {editmode? 
-                <input ref={editInput} value={task} onChange={updateText} className="task-input"></input>:
-                task.length > 0 ? <p>{task}</p> : <p>do something</p>   
+                <input ref={editInput} value={title} onChange={updateText} className="task-input"></input>:
+                <p className="task-title">{title.length > 0 ? title : "do something"}</p>
             }
             <div className="button-field">
-                <button onClick={toggleEditmode}>{editmode? <div>save</div> : <div>edit</div>}</button>
-                <button onClick={deleteTask}>delete</button>
+                <button className="task-button" onClick={toggleEditmode}>{editmode? <div>save</div> : <div>edit</div>}</button>
+                <button className="task-button" onClick={deleteTask}>delete</button>
             </div>
         </div>
     );
